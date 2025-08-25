483d2f29455fbe38aa48e05ed102d363e0c71807
v_czxgchen <v_czxgchen@tencent.com>
2025-08-25 17:24:45 +0800

    feat: demo6 列表定时轮询刷新+滚动加载

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import "./App.css";
 
 // import Page from "./demos/demo1"; // 元素拖拽
-import Page from "./demos/demo1 copy"; // 元素拖拽 优化
+// import Page from "./demos/demo1_refine"; // 元素拖拽 优化
 // import Page from "./demos/demo2"; // 滚动监听 和 tab 切换
 // import Page from "./demos/demo3"; // forwardRef 和 useImperativeHandle
 // import Page from "./demos/demo4"; // useTransition 和 useDeferredValue
 // import Page from "./demos/demo5"; // useSyncExternalStore 封装 localStorage 实现状态共享
-// import Page from "./demos/demo7"; // 列表定时轮询刷新+滚动加载
+import Page from "./demos/demo6"; // 列表定时轮询刷新+滚动加载
 
 if (process.env.NODE_ENV === "development") {
   require("./mockjs");
--- src/demos/demo6/index.tsx
+++ src/demos/demo6/index.tsx
@@ -1,9 +1,59 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
+import { Spin } from "antd";
+import InfiniteScroll from "react-infinite-scroll-component";
+import { useListStore } from "./store/list";
+import { getDataList, refreshDataList } from "./store/list/action";
 
-function TestComponent() {
-  return <div>TestComponent</div>;
+export default function DataList() {
+  const { dataList, total, getListLoading, hasMore, refreshLoading } =
+    useListStore();
+
+  const clearDataListTimer = useRef<() => void>(() => { });
+
+  const init = async () => {
+    await getDataList();
+
+    const dataListTimer = await refreshDataList();
+    clearDataListTimer.current = () => {
+      clearInterval(dataListTimer);
+    };
+  };
+
+  useEffect(() => {
+    (async () => {
+      await init();
+    })();
+    return () => {
+      clearDataListTimer.current();
+    };
+  }, []);
+
+  return (
+    <div>
+      <h2>Data List total: {total}</h2>
+      {refreshLoading && <p>Refreshing...</p>}
+      {hasMore && !getListLoading && (
+        <button onClick={getDataList}>Click：Load More</button>
+      )}
+      <div id='list' style={{ height: "400px", overflowY: "auto" }}>
+        <Spin spinning={getListLoading}>
+          <InfiniteScroll
+            dataLength={dataList.length}
+            next={getDataList}
+            hasMore={!!hasMore}
+            loader={null}
+            scrollableTarget={"list"}
+          >
+            {dataList.map((item) => (
+              <div key={item.id}>
+                <h3>{item.name}</h3>
+                <p>{item.description}</p>
+              </div>
+            ))}
+          </InfiniteScroll>
+          {!dataList.length && <p>No data</p>}
+        </Spin>
+      </div>
+    </div>
+  );
 }
-
-
-
-export default TestComponent;
